94ff5d0dcdc9153fc6c37c0fdcdf5314d558a4f0
Kemal Beganović <kemal.beganovic.24@size.ba>
2025-05-24 14:17:57 +0200

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,32 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {NavLink} from 'react-router-dom';
 import '../assets/styles/header.css'
 import logo from '../assets/images/logobg.png';
 
 function Header() {
-    return(
-        <header>
-            <nav>
-                <img src={logo} alt={"Logo"} className="logo"/>
-                <ul>
-                    <li><NavLink to="/" classname={({isActive}) => isActive ? 'active' : ''}>Početna</NavLink></li>
-                    <li><NavLink to="/about" classname={({isActive}) => isActive ? 'active' : ''}>O nama</NavLink></li>
-                    <li><NavLink to="/meni" classname={({isActive}) => isActive ? 'active' : ''}>Meni</NavLink></li>
-                    <li><NavLink to="/contact" classname={({isActive}) => isActive ? 'active' : ''}>Kontakt</NavLink></li>
-                    <li><NavLink to="order" classname={({isActive}) => isActive ? 'active' : ''}>Naruči</NavLink></li>
-                    <li><NavLink to="login" classname={({isActive}) => isActive ? 'active' : ''}>Prijava</NavLink></li>
+    const [scrolled, setScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setScrolled(window.scrollY > 50);
+        };
+
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
+
+    return (
+        <header className={`header ${scrolled ? "scrolled" : ""}`}>
+            <nav className="navbar">
+                <ul className="nav-list">
+                    <img src={logo} alt={"Logo"} className="logo"/>
+                    <li><NavLink to="" className={({isActive}) => isActive ? 'active' : ''}>Početna</NavLink></li>
+                    <li><NavLink to="/about" className={({isActive}) => isActive ? 'active' : ''}>O nama</NavLink></li>
+                    <li><NavLink to="/meni" className={({isActive}) => isActive ? 'active' : ''}>Meni</NavLink></li>
+                    <li><NavLink to="/contact" className={({isActive}) => isActive ? 'active' : ''}>Kontakt</NavLink>
+                    </li>
+                    <li><NavLink to="order" className={({isActive}) => isActive ? 'active' : ''}>Naruči</NavLink></li>
+                    <li><NavLink to="login" className={({isActive}) => isActive ? 'active' : ''}>Prijava</NavLink></li>
                 </ul>
             </nav>
         </header>
