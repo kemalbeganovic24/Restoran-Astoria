5d2185ed0df21e9a9e373cf7e7625eda5a572a35
Kemal Beganović <kemal.beganovic.24@size.ba>
2025-05-24 14:19:10 +0200

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 function Login(){
     return (
-        <h1>Prijavite se!</h1>
+        <div className="content">
+            <h1>Prijavite se!</h1>
+        </div>
     );
 }
 export default Login;
