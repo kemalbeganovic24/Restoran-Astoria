8414daa9acb038e126180adf7f276dcbd60dceb8
Kemal Beganović <kemal.beganovic.24@size.ba>
2025-05-24 14:18:51 +0200

    Update Contact.js

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 function Contact(){
     return (
-        <h1>Kako nas kontaktirati?</h1>
+        <div className="content">
+            <h1>Kako nas kontaktirati?</h1>
+        </div>
     );
 }
 export default Contact;
