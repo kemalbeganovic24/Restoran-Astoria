f3e619139839bfe123230e2c0d58c12621920824
Kemal Beganović <kemal.beganovic.24@size.ba>
2025-05-24 10:37:13 +0200

    Add files via upload

---
+++ components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../assets/styles/footer.css'
+function Footer() {
+    return(
+        <footer>
+            <p>&copy; Restoran Astorija - 2025 &copy;</p>
+        </footer>
+);
+}
+export default Footer;
