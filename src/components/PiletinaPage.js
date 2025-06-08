f8cd22db7cfd79da36d3486c2db2700130a4d5ed
Emir Đelilbašić <emir.djelilbasic.24@size.ba>
2025-06-08 18:41:30 +0200

    Update PiletinaPage.js

--- src/components/PiletinaPage.js
+++ src/components/PiletinaPage.js
@@ -23,10 +23,10 @@ function PiletinaPage() {
 
     return (
         <div className="meni-container">
+         <h2 className="section-title">Pileća Jela</h2>
             <button className="back-button" onClick={() => navigate('/meni')}>
                 Nazad na Meni
             </button>
-            <h2 className="section-title">Pileća Jela</h2>
             <div className="menu-cards-container">
                 {piletinaJela.map((jelo, index) => (
                     <MenuCard
