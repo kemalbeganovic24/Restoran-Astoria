04e9bed2f18db23c88d51d4d740c5f47b93dbd33
Emir Đelilbašić <emir.djelilbasic.24@size.ba>
2025-06-08 17:28:08 +0200

    Update Contact.js

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -55,28 +55,14 @@ function ContactForm(){
         <div className="form-container">
             <div className="form-info">
                 <h2>Imaš pitanje?</h2>
-                <p>Mi smo tu da pomognemo! Popuni odgovarajuća polja u formi ili nam se javi na broj telefona!</p>
-                <p> Uvijek se javljamo u rasponu od 8:00 sati do 19:00 sati!</p>
-                <p> restoranastroia@gmail.com</p>
-                <p> +387 062 470 002</p>
             </div>
             <form className="contact-form-grid" onSubmit={handleSubmit}>
 
                 <input name="name" type="text" placeholder="Ime" value={formData.name} onChange={(handleChange)} />
-                {errors.name && <span className="error">{errors.name}</span>}
-
                 <input name="lastName" type="text" placeholder="Prezime" value={formData.lastName} onChange={(handleChange) } />
-                {errors.lastName && <span className="error">{errors.lastName}</span>}
-
                 <input name="email" type="text" placeholder="Email" value={formData.email} onChange={(handleChange)} />
-                {errors.email && <span className="error">{errors.email}</span>}
-
                 <input name="phone" type="tel" placeholder="Broj Mobitela" maxLength={9} value={formData.phone} onChange={(handleChange)}/>
-                {errors.phone && <span className="error">{errors.phone}</span>}
-
-                <textarea name="message" placeholder="Vaša poruka" value={formData.message} onChange={(handleChange)} />
-                {errors.message && <span className="error">{errors.message}</span>}
-
+                <textarea name="message" placeholder="Vaša poruka" value={formData.message} onChange={(handleChange)} /> 
 
                 <button type="submit">Pošalji!</button>
             </form>
